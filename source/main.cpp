494cf378c625cf457f3caeb4a5db9d2c96206338
Chris DeJong <magiktrikdev@gmail.com>
2024-03-17 18:43:14 -0500

    POSY startup.

--- source/main.cpp
+++ source/main.cpp
@@ -1,40 +1,83 @@
-#include <core.h>
-#include <cli.h>
+//
+//
+//
 
+#include <core.h>
+
+#include <sys/stat.h>
 #include <cstdio>
 
-using namespace POSY;
+// --- Command Line Interactivity --------------------------------------
+//
+// A set of functions designed to help interact with the command line.
+//
+
+void
+cli_display_header()
+{
+    printf("\n                 ____   ___  ______   __\n");
+    printf("                |  _ \\ / _ \\/ ___\\ \\ / /\n");
+    printf("                | |_) | | | \\___ \\\\ V / \n");
+    printf("                |  __/| |_| |___) || |  \n");
+    printf("                |_|    \\___/|____/ |_|  \n\n");
+    printf("------------------------------------------------------------\n");
+    printf("  POSY               / Northern Illinois University\n");
+    printf("  Primary Maintainer / Chris DeJong\n");
+    printf("                     / Z1836870@students.niu.edu\n");
+    printf("                     / magiktrikdev@gmail.com\n");
+    printf("------------------------------------------------------------\n");
+    printf("  A parser for the COSY scripting language to C++.\n");
+    printf("  To properly use POSY, provide a list of source files to\n");
+    printf("  transpile and any additional parameters (if supported).\n");
+    printf("------------------------------------------------------------\n\n");
+    printf("  Useage: POSY [source files...]\n");
+    printf("     eg.: ./POSY test.foxy includes/ex.foxy includes/eg.foxy\n\n");
+}
+
+bool
+cli_validate_arguments(int argc, char ** argv)
+{
+
+    // Provides the user the necessary information to use the program.
+    if (argc <= 1)
+    {
+        cli_display_header();
+        return false;
+    }
+
+    // Cycles through all the arguments and determines if they're valid
+    // file paths. If any of the file paths are invalid, fast-exit and
+    // return false and indicate to the user that the path is invalid.
+    for (size_t idx = 1; idx < argc; ++idx)
+    {
+        struct stat file_stats = {};
+        if (stat(argv[idx], &file_stats) != 0)
+        {
+            cli_display_header();
+            printf("  Error: The file path \"%s\" is invalid.\n\n", argv[idx]);
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+// --- Entry Point -----------------------------------------------------
+//
+// Initiates the parser routine.
+//
 
 int
 main(int argc, char ** argv)
 {
 
-    // --- Command Line Argument Parsing -------------------------------
-    //
-    // Handles argument fetching, parsing, and grouping. Determines if
-    // user-provided paths are valid. Groups adjacent flags. Slightly
-    // over-engineered but designed to be extensible if desired.
-    //
+    // Validates the command line arguments.
+    if (cli_validate_arguments(argc, argv)) return 1;
 
-    // Construct the CLI arguments.
-    cli_node *command_line_args = cli_construct(argc, argv);
+    // All command line arguments are valid here, so we can now process
+    // them into memory.
 
-    // Cycle through the arguments.
-    cli_node *current = command_line_args;
-    while (current != NULL)
-    {
-        printf("%s\n", current->args.value);
-        current = current->next;
-    }
-
-    // --- Post Application Procedures ---------------------------------
-    //
-    // Here, anything that needs to get cleaned up will be processed here
-    // before main terminates.
-    //
-
-    cli_destruct(command_line_args);
-    smalloc_output_statistics();
     return 0;
 
 }
