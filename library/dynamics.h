dd239d16e7f604c4be7cc5d79741d2e14eb1e190
Chris DeJong <magiktrikdev@gmail.com>
2024-10-09 10:31:47 -0500

    Dynamic Types, copy constructor & equality?

--- library/dynamics.h
+++ library/dynamics.h
@@ -1,4 +1,4 @@
-// --- NDArray -----------------------------------------------------------------
+// -----------------------------------------------------------------------------
 //
 //      Northern Illinois University | Written by Chris DeJong 9/2024
 //
@@ -24,12 +24,231 @@
 
 #ifndef LIBRARY_DYNAMICS_H 
 #define LIBRARY_DYNAMICS_H 
+#include <vector>
 #include <cassert>
 #include <cstdint>
 #include <iostream>
 
 namespace Sigmafox
 {
+
+    // --- Utilities -----------------------------------------------------------
+    //
+    // Some utility classes for the Sigmafox library.
+    //
+
+    struct memory_stats
+    {
+        uint64_t bytes_allocated        = 0;
+        uint64_t bytes_freed            = 0;
+        uint64_t memory_allocations     = 0;
+        uint64_t memory_frees           = 0;
+    };
+
+    inline std::ostream& 
+    operator<<(std::ostream &os, const memory_stats& stats)
+    {
+        os << "Allocated: " << stats.bytes_allocated << " Freed: " << stats.bytes_freed;
+        os << " F/A Ratio: " << stats.memory_frees << "/" << stats.memory_allocations;
+        return os;
+    }
+
+    memory_stats *memory_get_stats() { static memory_stats _stats; return &_stats; }
+
+    struct memory_header
+    {
+        uint64_t amount_allocated;
+    };
+
+    inline void* 
+    memory_alloc(uint64_t size)
+    {
+
+        uint64_t size_required = sizeof(memory_header) + size;
+        void *region = malloc(size_required);
+        memory_header *header = (memory_header*)region;
+        header->amount_allocated = size;
+        void *user_offset = (uint8_t*)region + sizeof(memory_header);
+        memory_stats *stats = memory_get_stats();
+        stats->bytes_allocated += size;
+        stats->memory_allocations += 1;
+        return user_offset;
+
+    }
+
+    inline void
+    memory_free(void *buffer)
+    {
+
+        void *region = (uint8_t*)buffer - sizeof(memory_header);
+        memory_header *header = (memory_header*)region;
+        memory_stats *stats = memory_get_stats();
+        stats->bytes_freed += header->amount_allocated;
+        stats->memory_frees += 1;
+
+        free(region);
+
+    }
+
+    // --- N-Dimensional Arrays ------------------------------------------------
+    //
+    // Class definition for n-dimensional arrays.
+    // 
+    
+    template <typename T>
+    class NDArray
+    {
+
+        public:
+                            NDArray(std::initializer_list<size_t> dimensions);
+            virtual        ~NDArray();
+
+            T&              operator[](std::initializer_list<size_t> indices);
+            size_t          size() const;
+
+        protected:
+            size_t          compute_size() const;
+            size_t          compute_position(std::initializer_list<size_t> indices);
+
+            std::vector<size_t> dimensions;
+            std::vector<size_t> pitches;
+            T *packed_array = nullptr;
+
+    };
+
+    template <typename T> NDArray<T>::
+    NDArray(std::initializer_list<size_t> dimensions)
+    {
+
+        size_t size_required = 1;
+        for (const auto &d : dimensions)
+        {
+            size_required *= d;
+            this->dimensions.push_back(d);
+        }
+
+        void *region = malloc(size_required * sizeof(T));
+        this->packed_array = (T*)(region);
+        for (size_t idx = 0; idx < size_required; ++idx)
+        {
+            T* current_element = this->packed_array + idx;
+            current_element = new (current_element) T();
+        }
+
+        std::cout << "Array pitch(es) are: " << std::endl;
+        for (int32_t i = 0; i < this->dimensions.size(); ++i)
+        {
+            size_t pitch = 1;
+            for (int32_t j = i+1; j < this->dimensions.size(); ++j)
+            {
+                pitch *= this->dimensions[j];
+            }
+            std::cout << pitch << std::endl;
+            this->pitches.push_back(pitch);
+        }
+        
+    }
+
+    template <typename T> NDArray<T>::
+    ~NDArray()
+    {
+
+        if (packed_array != nullptr)
+        {
+
+            size_t elements = this->compute_size();
+            for (size_t idx = 0; idx < elements; ++idx)
+            {
+                T* current = this->packed_array + idx;
+                current->~T();
+            }
+
+            free(packed_array);
+        }
+
+    }
+
+    template <typename T> size_t NDArray<T>::
+    size() const
+    {
+
+        size_t result = this->compute_size();
+        return result;
+
+    }
+
+    template <typename T> size_t NDArray<T>::
+    compute_size() const
+    {
+
+        size_t size = 1;
+        for (const auto& d : this->dimensions) size *= d;
+        return size;
+
+    }
+
+    template <typename T> size_t NDArray<T>::
+    compute_position(std::initializer_list<size_t> indices)
+    {
+
+        size_t position = 0;
+        size_t index = 0;
+        for (const auto &i : indices)
+            index += this->pitches[position++] * i;
+        return index;
+
+    }
+
+    template <typename T> T& NDArray<T>::
+    operator[](std::initializer_list<size_t> indices)
+    {
+
+        size_t index = this->compute_position(indices);
+        T* location = this->packed_array + index;
+        return *location;
+
+    }
+
+    // --- Complex Type --------------------------------------------------------
+    //
+    // A numerical type with a real and imaginary components with all basic
+    // elementary operations therein.
+    //
+    // https://en.cppreference.com/w/cpp/language/user_literal
+    // We can apparently overload suffixes such that this statement becomes valid:
+    //      25.041 + 3.14i
+    //
+    // This would be a nice visual aid.
+    //
+
+    class Complex
+    {
+
+        public:
+                            Complex();
+                            Complex(double real);
+                            Complex(double real, double imaginary);
+            virtual        ~Complex();
+
+            inline Complex& operator+=(const Complex& rhs);
+            inline Complex& operator-=(const Complex& rhs);
+            inline Complex& operator*=(const Complex& rhs);
+            inline Complex& operator/=(const Complex& rhs);
+
+        protected:
+            double real_part;
+            double imaginary_part;
+
+    };
+
+    // --- Dynamic Type --------------------------------------------------------
+    //
+    // Type container class used during Sigmafox code generation for the transpiler.
+    // Generally, you wouldn't want to directly interface with this since it causes
+    // a bunch of runtime overhead when done incorrectly; so if you intend to actually
+    // develop with the Sigmafox core libraries, then you will want to use the actual
+    // types rather than the dynamic type itself.
+    //
 
     enum class DynamicTypetag
     {
@@ -44,11 +263,14 @@ namespace Sigmafox
         
         public:
             inline          Dynamic();
+            inline          Dynamic(const Dynamic& copy);
             inline          Dynamic(int init);
             inline          Dynamic(double init);
             inline          Dynamic(bool init);
             inline          Dynamic(const char *init);
             inline virtual ~Dynamic();
+
+            inline Dynamic& operator=(const Dynamic& rhs);
 
             template <typename T> T& value();
             template <typename T> T value() const;
@@ -102,9 +324,35 @@ namespace Sigmafox
 
             } break;
 
+            default:
+            {
+                
+                assert(!"Unimplemented OStream case.");
+
+            } break;
+
         };
 
         return os;
+
+    }
+
+    inline Dynamic::
+    Dynamic(const Dynamic& copy)
+    {
+
+        this->storage_size = copy.storage_size;
+        this->storage_buffer = malloc(this->storage_size);
+        this->tag = copy.get_tag();
+        memcpy(this->storage_buffer, copy.storage_buffer, this->storage_size);
+
+    }
+
+    inline Dynamic& Dynamic::
+    operator=(const Dynamic& rhs)
+    {
+
+        std::cout << "Equality: " << rhs << std::endl;
 
     }
 
--- library/dynamics_test.cpp
+++ library/dynamics_test.cpp
@@ -7,12 +7,23 @@ main(int argc, char ** argv)
 
     {
 
-        Sigmafox::Dynamic uninitialized;
+        // Dynamic casting to double.
         Sigmafox::Dynamic integer(12);
-        std::cout << integer << std::endl;
+        std::cout << integer.value<double>() << std::endl;
 
+        // Operator overloading.
         Sigmafox::Dynamic real_a(4);
         Sigmafox::Dynamic real_b(4);
+        Sigmafox::Dynamic real_result = real_a + real_b;
+        std::cout << "Result: " << real_result << std::endl;
+
+        // Tracked memory allocator for testing.
+        double* my_real = (double*)Sigmafox::memory_alloc(8);
+        *my_real = 25.55;
+        std::cout << *my_real << std::endl;
+        Sigmafox::memory_free(my_real);
+
+        std::cout << *Sigmafox::memory_get_stats() << std::endl;
 
     }
     
